9cc30db51221098d7d00fb3f67bf96f55d921ce9
Tomoya AMACHI <tomoya.amachi@gmail.com>
2020-06-03 13:27:36 +0900

    update cmakelists

--- example.cpp
+++ example.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 #include <cpr/cpr.h>
-//#include <nlohman/json.hpp>
+#include <nlohmann/json.hpp>
 
 
 int main(int argc, char** argv) {
     auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
-//    auto json = nlohmann::json::parse(response.text);
-    std::cout << response.text << std::endl;
-    //std::cout << json.dump(4) << std::endl;
+    auto json = nlohmann::json::parse(response.text);
+    //std::cout << response.text << std::endl;
+    std::cout << json.dump(4) << std::endl;
 }
